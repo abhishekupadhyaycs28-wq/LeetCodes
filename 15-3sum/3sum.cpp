0526c31afa45fcd37cf8eaaae4758c076e0bcb06
abhishekupadhyaycs28-wq <abhishek.upadhyay.cs28@iilm.edu>
2026-09-08 00:05:44 +0530

    Time: 48 ms (51.92%) | Memory: 29.1 MB (46.06%) - LeetSync

---
+++ 15-3sum/3sum.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+
+        vector<vector<int>> ans;
+        
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        int left = 0;
+        int right = 0;
+
+        for (int i = 0; i<n; i++) {
+
+            if (i>0 && nums[i]==nums[i-1]) {
+                continue;
+            }
+
+            left = i + 1;
+            right = n - 1;
+
+            while ( left < right ) {
+
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if ( sum < 0 ) left++;
+                else if ( sum > 0 ) right--;
+
+                else {
+
+                    ans.push_back({nums[i],nums[left],nums[right]});
+
+                    while (left<right && nums[right]==nums[right-1]) {
+
+                        right--;
+
+                    }
+                    while (left<right && nums[left]==nums[left+1]) {
+
+                        left++;
+
+                    }
+
+                    left++;
+                    right--;
+
+                }
+
+            }
+
+
+
+        }
+
+        return ans;
+
+    }
+};
